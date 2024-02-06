1a83c565bcefb971aec02e0fc92fe75b8a359387
Liz Nelson <121459797+Lizzard08@users.noreply.github.com>
2024-02-06 13:39:18 -0700

    Add files via upload

---
+++ mp_parsetree.py
@@ -0,0 +1,33 @@
+from binarytree import BinaryTree
+from stack import Stack
+import unittest
+
+
+def buildMPLogicParseTree(s):
+    """ ADD ANY DOCUMENTATION HERE, AND IMPLEMENTATION BELOW """
+    pass
+
+def evaluateMPLogicParseTree(t):
+    """ ADD ANY DOCUMENTATION HERE, AND IMPLEMENTATION BELOW """
+    pass
+
+def printMPLogicExpression(t):
+    """ ADD ANY DOCUMENTATION HERE, AND IMPLEMENTATION BELOW """
+    pass
+
+
+class TestParseTree(unittest.TestCase):
+    """ ADD METHODS TO TEST PARSE/EVAL/PRINT FUNCTIONS """
+    pass
+
+def unittest_main():
+    unittest.main()
+
+
+def main():
+    pass
+
+# only executed if run as a standalone program
+if __name__ == '__main__':
+    main()
+    unittest_main()
